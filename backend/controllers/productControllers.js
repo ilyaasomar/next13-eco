f3149ffb1d72295d1008b91b371776b9eddbb306
ilyaas <Ilyaas Omar>
2023-03-25 15:01:53 +0300

    L5

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -7,3 +7,10 @@ export const getProducts = async (req, res, next) => {
   const products = await Product.find();
   res.status(200).json({ products });
 };
+export const getProduct = async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+  if (!product) {
+    res.status(404).json({ error: "Product Not Found" });
+  }
+  res.status(200).json({ product });
+};
