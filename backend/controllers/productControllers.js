609b60e6fc7b885b716f8a7aff31501600b24121
ilyaas <ilyaascumar10@gmail.com>
2023-03-29 10:30:57 +0300

    backend completed

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -5,9 +5,20 @@ export const newProduct = async (req, res, next) => {
   res.status(201).json({ product });
 };
 export const getProducts = async (req, res, next) => {
-  const apiFilters = new APIFilters(Product.find(), req.query).search();
-  const products = await apiFilters.query;
-  res.status(200).json({ products });
+  const resPerPage = 3;
+  const productsCount = await Product.countDocuments();
+  const apiFilters = new APIFilters(Product.find(), req.query)
+    .search()
+    .filters();
+  let products = await apiFilters.query;
+
+  const filteredProductCount = products.length;
+  apiFilters.pagination(resPerPage);
+  products = await apiFilters.query.clone();
+  // clone() : is mongoose function that uses when your want to execute queries more than once
+  res
+    .status(200)
+    .json({ productsCount, resPerPage, filteredProductCount, products });
 };
 export const getProduct = async (req, res, next) => {
   const product = await Product.findById(req.query.id);
--- backend/utils/APIFilters.js
+++ backend/utils/APIFilters.js
@@ -3,6 +3,7 @@ class APIFilters {
     this.query = query;
     this.queryStr = queryStr;
   }
+  // search by name
   search() {
     const keyword = this.queryStr.keyword
       ? {
@@ -14,6 +15,49 @@ class APIFilters {
       : {};
     console.log(keyword);
     this.query = this.query.find({ ...keyword });
+    return this;
+  }
+  // filters category and price
+  filters() {
+    const queryCopy = { ...this.queryStr };
+    const removeFields = ["keyword", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    // min and max price code are here
+    // greater than and less than in express are simple but nextjs you making as manual for js code
+    let output = {};
+    let prop = "";
+    for (let key in queryCopy) {
+      console.log({ key: key });
+      if (!key.match(/\b(gt|gte|lt|lte)/)) {
+        output[key] = queryCopy[key];
+      } else {
+        prop = key.split("[")[0];
+        console.log({ prop: prop });
+        let operator = key.match(/\[(.*)\]/)[1];
+        console.log({ operator: operator });
+        if (!output[prop]) {
+          output[prop] = {};
+        }
+        output[prop][`$${operator}`] = queryCopy[key];
+      }
+    }
+    console.log("output", output);
+    // {price : { $gte : 100, $lte : 1000 }}
+    this.query = this.query.find(output);
+    return this;
+  }
+  // pagination
+  pagination(resPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+    // if your need to go page 2
+    // resPerPage = 5 means all pages of the product
+    // currentPage = 2
+    // currentPage->2 - 1 = 1 * resPerPage->5 = 5 means skip that 5 product from that page
+    // and go page 2 from product 6 to 10
+
+    this.query = this.query.limit(resPerPage).skip(skip);
     return this;
   }
 }
